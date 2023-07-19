6124b6eab7a2cca895927263721a66b57af004f8
Aaron Trelstad <trelstadaaron@gmail.com>
2023-07-19 13:02:43 -0500

    stack

--- unit 13/stack.cpp
+++ unit 13/stack.cpp
@@ -1,10 +1,39 @@
 #include <iostream>
 #include <vector>
 
+template <typename T>
 class Stack {
-    std::vector<int> nums;
+    private:
+        std::vector<T> array;
 
     public: 
-    
+        bool isEmpty() {
+            return array.empty();
+        }
 
+        void push(int value) {
+            array.push_back(value);
+            std::cout << "Pushed element: " << value << '\n';
+        }
+
+        void pop() {
+            if (isEmpty()) {
+                std::cout << "Cannot Pop, Stack is empty" << '\n';
+                return;
+            }
+
+            T value = array.back();
+            array.pop_back();
+            std::cout << "Popped element: " << value << '\n';
+        }
 };
+
+int main() {
+    Stack<int> stack;
+
+    stack.push(4);
+    stack.push(5);
+    stack.pop();
+
+    return 0;
+}
